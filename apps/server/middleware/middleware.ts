f04762571e27e29555d6ca0c63e80f5b115a768f
0xdolores <rubairakib11@gmail.com>
2025-12-25 19:24:16 +0600

    feat: Add middleware and user routes

---
+++ apps/server/middleware/middleware.ts
@@ -0,0 +1,31 @@
+import type { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+
+export async function authMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    const token: string = req.cookies.token;
+    if (token) {
+      jwt.verify(
+        token,
+        process.env.USER_JWT_SECRET ?? "secret",
+        (err, decoded) => {
+          if (err) {
+            res.status(403).json({ error: err.message });
+          } else {
+            //@ts-ignore
+            req.decodedUser = decoded;
+            next();
+          }
+        }
+      );
+    } else {
+      res.status(403).json({ message: "No token provide" });
+    }
+  } catch (error: any) {
+    console.log(error);
+  }
+}
